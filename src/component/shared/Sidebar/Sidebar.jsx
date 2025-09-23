50291950dfcbd7b7dd48bedc1386dae9d52be7b3
HirendraGurnani <ghiren20@gmail.com>
2025-09-23 18:17:12 +0530

    fix: add chamber icon updated

--- src/component/shared/Sidebar/Sidebar.jsx
+++ src/component/shared/Sidebar/Sidebar.jsx
@@ -114,7 +114,7 @@ const Sidebar = ({ open, onClose }) => {
           <li className="nav-item">
             <NavLink className="nav-link" to="dry-warehouse/create-chamber">
               <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                <svg
+                {/* <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="18"
                   height="18"
@@ -135,6 +135,20 @@ const Sidebar = ({ open, onClose }) => {
                   ></rect>
                   <path d="M7 10V6a5 5 0 0 1 10 0v4"></path>
                   <circle cx="12" cy="15" r="2"></circle>
+                </svg> */}
+                <svg
+                width="16"
+                  viewBox="0 -0.5 24 24"
+                  id="meteor-icon-kit__solid-warehouse"
+                  fill="none"
+                  // xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M4 23V10C4 9.4477 4.44772 8.99999 5 8.99999H19C19.5523 8.99999 20 9.4477 20 10V23H4zM18 11H6V23H18V11zM1 23C0.447715 23 0 22.5523 0 22V6.6439C0 5.85962 0.458406 5.14771 1.17239 4.82317L11.1724 0.27772C11.6982 0.03869 12.3018 0.03869 12.8276 0.27772L22.8276 4.82317C23.5416 5.14771 24 5.85962 24 6.6439V22C24 22.5523 23.5523 23 23 23H1zM6 13H18V15H6V13zM6 21H18V23H6V21zM6 17H18V19H6V17z"
+                    fill="currentColor"
+                  />
                 </svg>
               </div>
               <span className="nav-link-text ms-1">Add Chamber</span>
