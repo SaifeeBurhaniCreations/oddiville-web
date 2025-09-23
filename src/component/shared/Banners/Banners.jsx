26963d71a037f69ccfb51624ca7c10a6e4f91b98
reeta733 <reetasiya@gmail.com>
2025-09-23 18:21:08 +0530

    Working on banner saction but  not yet completed

--- src/component/shared/Banners/Banners.jsx
+++ src/component/shared/Banners/Banners.jsx
@@ -2,28 +2,28 @@ import { useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
 const Banners = ({
-  fetchBanners,
+  form,
   getBanners,
   deleteBanners,
   setDeleteBanners,
-  formError,
-  formTouched,
+  // formError,
+  // formTouched,
   name = "upload banner",
   onFileChange,
   setFieldValue,
 }) => {
   const bannerRef = useRef();
+
   const [banner, setBanner] = useState({
     banner: null,
     preview: "",
   });
   const [typeCheckError, setTypeCheckError] = useState("");
 
+
   const updateBanner = (file) => {
     if (!file) return;
-    // if (!["image/png", "image/jpeg"].includes(file.type)) {
-    //   setTypeCheck("Only PNG and JPG formats allowed");
-    // } else {
+
     const reader = new FileReader();
     reader.onload = (e) => {
       const arr = new Uint8Array(e.target.result).subarray(0, 4);
@@ -41,148 +41,118 @@ const Banners = ({
             banner: file,
             preview: previewReader.result,
           });
-          if(setFieldValue){
-            setFieldValue("sample_image", file)
+
+          if (setFieldValue) {
+            setFieldValue("sample_image", file);
           }
         };
         previewReader.readAsDataURL(file);
+
         if (onFileChange) {
           onFileChange({ target: { files: [file] } });
         }
       } else {
         setBanner({ banner: null, preview: "" });
         setTypeCheckError("Only valid PNG and JPG images are allowed");
-        if (bannerRef.current) {
+
+        if (bannerRef?.current) {
           bannerRef.current.value = "";
         }
-        if(setFieldValue){
-          setFieldValue("sample_image", null)
+
+        if (setFieldValue) {
+          setFieldValue("sample_image", null);
         }
       }
     };
 
     reader.readAsArrayBuffer(file.slice(0, 4));
-    // }
   };
 
-
   useEffect(() => {
-    fetchBanners(banner.banner);
+    // fetchBanners(banner.banner);
   }, [banner]);
 
   return (
     <>
-      {/* <div className="card my-3"> */}
-      {/* <div className="card-header pt-4 pb-2">
-          <div className="flex-cs header">
-            <h6>
-              Add Banner
-              <span data-tooltip="Preview">
-                <button
-                  className="cs-btn"
-                  type="button"
-                  data-bs-toggle="modal"
-                  data-bs-target="#modal-banner"
-                >
-                  <i
-                    className="fa-regular fa-lg fa-circle-info"
-                    style={{ color: "#aaa" }}
-                  />
-                </button>
-              </span>
-            </h6>
-          </div>
-        </div> */}
-      {/* <div className="card-body"> */}
       <input
-        className="hide-me"
-        accept="image/png, image/jpeg"
         type="file"
+        name="sample_image"
+        accept="image/png, image/jpeg"
         ref={bannerRef}
         onChange={(e) => updateBanner(e.target.files[0])}
         style={{ visibility: "hidden" }}
       />
+      <label htmlFor="">
+        {name}
+        {form.errors.sample_image && (
+          <span className="text-danger fw-normal fs-error">
+            &emsp;{form.errors.sample_image}
+          </span>
+        )}
+      </label>
+
       <div className="projects-banners">
         {getBanners ? (
-          <>
-            <div className="mb-2 w-100 header">
-              {/* <h5 className="m-0">Add Banner</h5> */}
-            </div>
-            <div
-              className={`layout ${
-                deleteBanners?.includes(getBanners?.key) ? "fade-cs" : ""
-              }`}
-            >
-              {banner?.preview ? (
-                <button
-                  type="button"
-                  onClick={() => setBanner({ banner: null, preview: "" })}
-                  className="btn remove-btn-cs bg-gradient-danger mt-2"
-                >
-                  Remove
-                </button>
-              ) : (
+          <div
+            className={`layout ${
+              deleteBanners?.includes(getBanners?.key) ? "fade-cs" : ""
+            }`}
+          >
+            {banner?.preview ? (
+              <button
+                type="button"
+                onClick={() => setBanner({ banner: null, preview: "" })}
+                className="btn remove-btn-cs bg-gradient-danger mt-2"
+              >
+                Remove
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={() => bannerRef.current?.click()}
+                className="btn remove-btn-cs bg-gradient-success m-0"
+              >
+                Change
+              </button>
+            )}
+            {banner?.preview ? (
+              <img src={banner.preview} alt="Preview" />
+            ) : (
+              <img src={getBanners?.url} alt="" />
+            )}
+          </div>
+        ) : (
+          <div className="layout">
+            {banner?.preview && (
+              <button
+                type="button"
+                onClick={() => setBanner({ banner: null, preview: "" })}
+                className="btn bg-gradient-danger remove-btn-cs mt-2"
+              >
+                Remove
+              </button>
+            )}
+            {banner.banner ? (
+              <img src={banner.preview} alt="Preview" />
+            ) : (
+              <div className="d-flex flex-column">
                 <button
                   type="button"
                   onClick={() => bannerRef.current?.click()}
-                  className="btn remove-btn-cs bg-gradient-success m-0"
+                  className="btn btn-default"
                 >
-                  change
+                  <i className="fa-solid fa-plus" /> &nbsp; {name}
                 </button>
-              )}
-              {banner?.preview ? (
-                <img src={banner.preview} alt="Preview" />
-              ) : (
-                <img src={getBanners?.url} alt="" />
-              )}
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="mb-2 w-100 header">
-              {/* <h5 className="m-0">Add Banner</h5> */}
-            </div>
-            <div className="layout">
-              {banner?.preview && (
-                <button
-                  type="button"
-                  onClick={() => setBanner({ banner: null, preview: "" })}
-                  className="btn bg-gradient-danger remove-btn-cs mt-2"
-                >
-                  Remove
-                </button>
-              )}
-              {banner.banner ? (
-                <img src={banner.preview} alt="Preview" />
-              ) : (
-                <div className="d-flex flex-column">
-                  <button
-                    type="button"
-                    onClick={() => bannerRef.current?.click()}
-                    className={
-                      "btn btn-default " +
-                      (formError && formTouched
-                        ? "border border-danger text-danger"
-                        : "")
-                    }
-                  >
-                    <i className="fa-solid fa-plus" /> &nbsp; {name}
-                  </button>
-                  {formError && formTouched ? (
-                    <sub className="text-danger text-center">{formError}</sub>
-                  ) : (
-                    <sub className="text-danger text-center">
-                      {typeCheckError}
-                    </sub>
-                  )}
-                </div>
-              )}
-            </div>
-          </>
+                {typeCheckError && (
+                  <sub className="text-danger text-center">
+                    {typeCheckError}
+                  </sub>
+                )}
+              </div>
+            )}
+          </div>
         )}
       </div>
-      {/* </div> */}
-      {/* </div> */}
     </>
   );
 };
