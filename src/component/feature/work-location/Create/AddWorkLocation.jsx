0b378ea532efea69916b634f7e59a50d08705da4
HirendraGurnani <ghiren20@gmail.com>
2025-09-25 18:04:07 +0530

    fix: validation on location component fixed for now. still some work left

--- src/component/feature/work-location/Create/AddWorkLocation.jsx
+++ src/component/feature/work-location/Create/AddWorkLocation.jsx
@@ -1,3 +1,4 @@
+import { useFormik } from "formik";
 import { useEffect, useState } from "react";
 import { create, modify } from "../../../../services/WorkLocatonService";
 import Spinner from "../../../shared/Spinner/Spinner";
@@ -9,18 +10,25 @@ import {
   handlePostData,
 } from "../../../../redux/WorkLocationSlice";
 import Banners from "../../../shared/Banners/Banners";
-import { useFormValidator } from "../../../../../custom_library/formValidator/useFormValidator";
+import { useFormValidator } from ".././../../../../custom_library/formValidator/useFormValidator";
 
 const AddWorkLocation = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { id } = useParams();
+  const param = useParams();
+  const { id } = param;
 
   const workLocation = useSelector((state) => state.location.data);
 
   const [isLoading, setIsLoading] = useState(false);
-  const [fetchedBanners, setFetchedBanners] = useState(null);
-  const [deleteBanners, setDeleteBanners] = useState(null);
+  const [banners, setBanners] = useState();
+  const [fetchedBanners, setFetchedBanners] = useState();
+  const [deleteBanners, setDeleteBanners] = useState();
+  // const [initialValues, setInitialValues] = useState({
+  //   location_name: '',
+  //   description: '',
+  //   sample_image: null
+  // })
 
   const form = useFormValidator(
     {
@@ -36,139 +44,175 @@ const AddWorkLocation = () => {
         { type: "required", message: "Description is required" },
         { type: "minLength", length: 5, message: "Minimum 5 characters" },
       ],
-      sample_image: [{ type: "required", message: "Image is required" }],
+      sample_image: [
+        {
+          type: "required",
+          message: "Image is required",
+          // condition: () => !id,
+        },
+      ],
     },
     { validateOnChange: true, debounce: 300 }
   );
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // const formData = new FormData();
+    // formData.append("location_name", form?.values?.location_name)
+    // formData.append("description", form?.values?.description)
+    // if(banners){
+    //   formData.append("sample_image", form?.values?.sample_image)
+    // }
+
+    if(banners){
+      console.log(banners);
+      
+      // form.setField("sample_image", banners)
+    }
+    const res = form.validateForm();
+    console.log(res);
+    
+  };
+
+
+  // const formFormik = useFormik({
+  //   initialValues,
+  //   onSubmit: async (formData) => {
+  //     const formPayload = new FormData();
+
+  //     // Add basic fields
+  //     formPayload.append("location_name", formData.location_name);
+  //     formPayload.append("description", formData.description);
+
+  //     // Add sample_image if exists
+  //     if (banners) {
+  //       formPayload.append("sample_image", banners);
+  //     }
+
+  //     // // Log the formPayload
+  //     // const payloadObject = {};
+  //     // formPayload.forEach((value, key) => {
+  //     //   payloadObject[key] = value;
+  //     // });
+  //     // console.log("Form Payload:", payloadObject); // Log the payload
+
+  //     // return
+
+  //     setIsLoading(true);
+  //     try {
+  //       if (!id) {
+  //         const response = await create(formPayload);
+  //         if (response.status === 201) {
+  //           dispatch(handlePostData(response.data));
+  //           navigate("/work-location");
+  //           toast.success("location is Added !!");
+  //         } else {
+  //           toast.error("Failed to create location.");
+  //         }
+  //       } else {
+  //         // Add deleteBanner flag if updating
+  //         // if (deleteBanner) {
+  //         //   formPayload.append("deleteBanner", "true");
+  //         // }
+
+  //         const response = await modify({ formData: formPayload, id });
+  //         if (response.status === 200) {
+  //           dispatch(handleModifyData(response.data));
+  //           navigate("/work-location");
+  //           toast.success("location is Updated !!");
+  //         } else {
+  //           toast.error("Failed to update location.");
+  //         }
+  //       }
+  //     } catch (error) {
+  //       toast.error("An error occurred while processing the location.");
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   },
+  // });
 
   useEffect(() => {
     if (id) {
       const data = workLocation?.find((value) => value.id === id);
-      if (data) {
-        form.setField("location_name", data.location_name);
-        form.setField("description", data.description);
-        setFetchedBanners(data?.sample_image);
-      }
+      form.setField(data);
+      setFetchedBanners(data?.sample_image);
     }
-  }, [id, workLocation]);
+  }, [id]);
+
+  const fetchBanners = (data) => {
+    setBanners(data);
+  };
 
   const handleExit = () => {
     setFetchedBanners(null);
-    form.setField("location_name", "");
-    form.setField("description", "");
-    form.setField("sample_image", null);
+    form.setFields({ location_name: "", description: "", sample_image: null });
     navigate("/work-location");
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const result = form.validateForm();
-    if (!result.success) return;
-
-    const formPayload = new FormData();
-    
-    formPayload.append("location_name", form.values.location_name);
-    formPayload.append("description", form.values.description);
-    
-    if (form.values.sample_image) {
-      formPayload.append("sample_image", form.values.sample_image);
-    }
-    
-    console.log(formPayload);
-    setIsLoading(true);
-    try {
-      if (!id) {
-        const response = await create(formPayload);
-        if (response.status === 201) {
-          dispatch(handlePostData(response.data));
-          navigate("/work-location");
-          toast.success("Location is Added!");
-        } else {
-          toast.error("Failed to create location.");
-        }
-      } else {
-        const response = await modify({ formData: formPayload, id });
-        if (response.status === 200) {
-          dispatch(handleModifyData(response.data));
-          navigate("/work-location");
-          toast.success("Location is Updated!");
-        } else {
-          toast.error("Failed to update location.");
-        }
-      }
-    } catch (error) {
-      toast.error("An error occurred while processing the location.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="card">
-        <div className="card-header flex-cs gap-2 justify-content-between pt-4 pb-2">
-          <h6>Manage Work Locations</h6>
-          {id && (
-            <button
-              type="button"
-              className="btn btn-sm btn-secondary"
-              onClick={handleExit}
-            >
-              Clear
-            </button>
-          )}
-        </div>
-        <div className="card-body grid-cs gtc-1 pb-4">
-          <Banners
-            name="Upload Location"
-            getBanners={fetchedBanners}
-            deleteBanners={deleteBanners}
-            setDeleteBanners={setDeleteBanners}
-            form={form}
-            setFieldValue={form.setField}
-          />
-          {console.log(form.values)}
-          <div className="grid-cs gtc-1">
-            <div>
-              <input
-                type="text"
-                value={form.values.location_name}
-                onChange={(e) => form.setField("location_name", e.target.value)}
-                className="form-control"
-                name="location_name"
-                placeholder="Location Name"
-              />
-              {form.errors.location_name && (
-                <span className="text-danger">{form.errors.location_name}</span>
-              )}
-            </div>
-            <div>
-              <input
-                type="text"
-                value={form.values.description}
-                onChange={(e) => form.setField("description", e.target.value)}
-                className="form-control"
-                name="description"
-                placeholder="Service Description"
-              />
-              {form.errors.description && (
-                <span className="text-danger">{form.errors.description}</span>
-              )}
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="card">
+          <div className="card-header flex-cs gap-2 justify-content-between pt-4 pb-2">
+            <h6>Manage Work Locations</h6>
+            {id && (
+              <button
+                type="button"
+                className="btn btn-sm btn-secondary"
+                onClick={handleExit}
+              >
+                Clear
+              </button>
+            )}
+          </div>
+          <div className="card-body grid-cs gtc-1 pb-4">
+            <Banners
+              name="Uplaod Location"
+              form={form}
+              getBanners={fetchedBanners}
+              deleteBanners={deleteBanners}
+              setDeleteBanners={setDeleteBanners}
+              fetchBanners={fetchBanners}
+            />
+            <div className="grid-cs gtc-1">
+              <div>
+                <input
+                  type="text"
+                  value={form?.values?.location_name}
+                  // onChange={form.handleChange}
+                  onChange={(e)=>form.setField("location_name", e.target.value)}
+                  className="form-control"
+                  name="location_name"
+                  placeholder="Location Name"
+                  id=""
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  value={form?.values?.description}
+                  // onChange={form.handleChange}
+                  onChange={(e)=>form.setField("description", e.target.value)}
+                  className="form-control"
+                  name="description"
+                  placeholder="Service Description"
+                  id=""
+                />
+              </div>
             </div>
           </div>
+          <div className="card-footer">
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="btn btn-primary btn-md m-0"
+            >
+              {id ? "Update" : "Save"} Location {isLoading && <Spinner />}
+            </button>
+          </div>
         </div>
-        <div className="card-footer">
-          <button
-            type="submit"
-            // disabled={!form.isValid || isLoading}
-            className="btn btn-primary btn-md m-0"
-          >
-            {id ? "Update" : "Save"} Location {isLoading && <Spinner />}
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
--- src/component/shared/Banners/Banners.jsx
+++ src/component/shared/Banners/Banners.jsx
@@ -2,30 +2,29 @@ import { useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
 const Banners = ({
-  form,
+  fetchBanners,
   getBanners,
   deleteBanners,
   setDeleteBanners,
-  // formError,
-  // formTouched,
   name = "upload banner",
   onFileChange,
-  setFieldValue,
+  form,
 }) => {
   const bannerRef = useRef();
-
   const [banner, setBanner] = useState({
     banner: null,
     preview: "",
   });
-  const [typeCheckError, setTypeCheckError] = useState("");
-
 
   const updateBanner = (file) => {
     if (!file) return;
-
+    // if (!["image/png", "image/jpeg"].includes(file.type)) {
+    //   setTypeCheck("Only PNG and JPG formats allowed");
+    // } else {
     const reader = new FileReader();
     reader.onload = (e) => {
+      // console.log(e.target.result); return
+
       const arr = new Uint8Array(e.target.result).subarray(0, 4);
       let header = "";
       for (let i = 0; i < arr.length; i++) {
@@ -41,118 +40,140 @@ const Banners = ({
             banner: file,
             preview: previewReader.result,
           });
-
-          if (setFieldValue) {
-            setFieldValue("sample_image", file);
-          }
+          // if (form.setField) {
+          //   form.setField("sample_image", file);
+          // }
         };
         previewReader.readAsDataURL(file);
-
         if (onFileChange) {
           onFileChange({ target: { files: [file] } });
         }
       } else {
         setBanner({ banner: null, preview: "" });
         setTypeCheckError("Only valid PNG and JPG images are allowed");
-
-        if (bannerRef?.current) {
+        if (bannerRef.current) {
           bannerRef.current.value = "";
         }
-
-        if (setFieldValue) {
-          setFieldValue("sample_image", null);
+        if (form.setField) {
+          form.setField("sample_image", null);
         }
       }
     };
 
     reader.readAsArrayBuffer(file.slice(0, 4));
+    // }
   };
 
   useEffect(() => {
-    // fetchBanners(banner.banner);
+    fetchBanners(banner.banner);
   }, [banner]);
 
   return (
     <>
+      {/* <div className="card my-3"> */}
+      {/* <div className="card-header pt-4 pb-2">
+          <div className="flex-cs header">
+            <h6>
+              Add Banner
+              <span data-tooltip="Preview">
+                <button
+                  className="cs-btn"
+                  type="button"
+                  data-bs-toggle="modal"
+                  data-bs-target="#modal-banner"
+                >
+                  <i
+                    className="fa-regular fa-lg fa-circle-info"
+                    style={{ color: "#aaa" }}
+                  />
+                </button>
+              </span>
+            </h6>
+          </div>
+        </div> */}
+      {/* <div className="card-body"> */}
       <input
+        className="hide-me"
         type="file"
-        name="sample_image"
         accept="image/png, image/jpeg"
         ref={bannerRef}
-        onChange={(e) => updateBanner(e.target.files[0])}
+        onChange={(e) => {
+          updateBanner(bannerRef.current.files[0]);
+          console.log(bannerRef.current.files[0]);
+          form.setField("sample_image", e.target.files[0].name)
+        }}
         style={{ visibility: "hidden" }}
       />
-      <label htmlFor="">
-        {name}
-        {form.errors.sample_image && (
-          <span className="text-danger fw-normal fs-error">
-            &emsp;{form.errors.sample_image}
-          </span>
-        )}
-      </label>
-
       <div className="projects-banners">
         {getBanners ? (
-          <div
-            className={`layout ${
-              deleteBanners?.includes(getBanners?.key) ? "fade-cs" : ""
-            }`}
-          >
-            {banner?.preview ? (
-              <button
-                type="button"
-                onClick={() => setBanner({ banner: null, preview: "" })}
-                className="btn remove-btn-cs bg-gradient-danger mt-2"
-              >
-                Remove
-              </button>
-            ) : (
-              <button
-                type="button"
-                onClick={() => bannerRef.current?.click()}
-                className="btn remove-btn-cs bg-gradient-success m-0"
-              >
-                Change
-              </button>
-            )}
-            {banner?.preview ? (
-              <img src={banner.preview} alt="Preview" />
-            ) : (
-              <img src={getBanners?.url} alt="" />
-            )}
-          </div>
-        ) : (
-          <div className="layout">
-            {banner?.preview && (
-              <button
-                type="button"
-                onClick={() => setBanner({ banner: null, preview: "" })}
-                className="btn bg-gradient-danger remove-btn-cs mt-2"
-              >
-                Remove
-              </button>
-            )}
-            {banner.banner ? (
-              <img src={banner.preview} alt="Preview" />
-            ) : (
-              <div className="d-flex flex-column">
+          <>
+            <div className="mb-2 w-100 header">
+              {/* <h5 className="m-0">Add Banner</h5> */}
+            </div>
+            <div
+              className={`layout ${
+                deleteBanners?.includes(getBanners?.key) ? "fade-cs" : ""
+              }`}
+            >
+              {banner?.preview ? (
+                <button
+                  type="button"
+                  onClick={() => setBanner({ banner: null, preview: "" })}
+                  className="btn remove-btn-cs bg-gradient-danger mt-2"
+                >
+                  Remove
+                </button>
+              ) : (
                 <button
                   type="button"
                   onClick={() => bannerRef.current?.click()}
-                  className="btn btn-default"
+                  className="btn remove-btn-cs bg-gradient-success m-0"
+                >
+                  change
+                </button>
+              )}
+              {banner?.preview ? (
+                <img src={banner.preview} alt="Preview" />
+              ) : (
+                <img src={getBanners?.url} alt="" />
+              )}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="mb-2 w-100 header">
+              {/* <h5 className="m-0">Add Banner</h5> */}
+            </div>
+            <div className="layout">
+              {banner?.preview && (
+                <button
+                  type="button"
+                  onClick={() => setBanner({ banner: null, preview: "" })}
+                  className="btn bg-gradient-danger remove-btn-cs mt-2"
+                >
+                  Remove
+                </button>
+              )}
+              {banner.banner ? (
+                <img src={banner.preview} alt="Preview" />
+              ) : (
+                <button
+                  type="button"
+                  onClick={() => bannerRef.current?.click()}
+                  className={
+                    "btn btn-default" +
+                    (form.errors.sample_image && " border border-danger")
+                  }
                 >
                   <i className="fa-solid fa-plus" /> &nbsp; {name}
                 </button>
-                {typeCheckError && (
-                  <sub className="text-danger text-center">
-                    {typeCheckError}
-                  </sub>
-                )}
-              </div>
-            )}
-          </div>
+              )}
+            </div>
+          </>
         )}
       </div>
+      {/* </div> */}
+      {/* </div> */}
     </>
   );
 };
